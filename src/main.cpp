200f6664b4020ab7e2916f3a113a51cffcb6d1ce
Msfv3n0m <71530214+Msfv3n0m@users.noreply.github.com>
2023-07-06 20:16:27 +0000

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int DisplayResourceNAMessageBox()
+{
+    int msgboxID = MessageBoxW(
+        NULL,
+        (LPCWSTR)L"Resource not available\nDo you want to try again?",
+        (LPCWSTR)L"Account Details",
+        MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2
+    );
+
+    switch (msgboxID)
+    {
+    case IDCANCEL:
+        // TODO: add code
+        break;
+    case IDTRYAGAIN:
+        // TODO: add code
+        break;
+    case IDCONTINUE:
+        // TODO: add code
+        break;
+    }
+
+    return msgboxID;
+}
+
+int main() {
+    DisplayResourceNAMessageBox();
+    return 0;
+}
